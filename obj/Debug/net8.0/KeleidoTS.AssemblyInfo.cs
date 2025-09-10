d29586b2b07b62255ea93aacf707b9c0bdecce71
tuurash <turasmimoo@gmail.com>
2025-09-11 03:48:22 +0600

    dsa

--- obj/Debug/net8.0/KeleidoTS.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeleidoTS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeleidoTS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0298c49812bb79aa2bb26717d281da5a7efb99d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd278b05366bc732e63d95a9e70d6c2efd0e984f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeleidoTS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeleidoTS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
